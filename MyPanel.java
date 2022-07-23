2e99df33765cc5d902466ee9d801deab255f1e36
JustusSager <justus.sager@gmail.com>
2022-07-23 18:59:30 +0200

    QuadTree in MyPanel zeichnen

--- MyPanel.java
+++ MyPanel.java
@@ -17,10 +17,10 @@ public class MyPanel extends JPanel implements ActionListener{
 
         quadtree = new QuadTree(0, 0, PANEL_WIDTH, PANEL_HEIGHT, 4);
 
-        points = new Point[100];
+        points = new Point[300];
 
         for (int i = 0; i < points.length; i++) {
-            points[i] = new Point(Math.random() * PANEL_WIDTH, Math.random() * PANEL_HEIGHT, null);
+            points[i] = new Point(Math.random() * PANEL_WIDTH, Math.random() * PANEL_HEIGHT, points[i]);
         }
 
         for(Point point : points) {
@@ -30,13 +30,27 @@ public class MyPanel extends JPanel implements ActionListener{
 
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
         super.paint(g);
-        quadtree.draw(g2d);
+
+        this.draw_quadtree(g2d, this.quadtree);
 
         g2d.setColor(Color.GREEN);
         for (Point point : points) {
             g2d.fillOval((int) point.x - 2, (int) point.y - 2, 4, 4);
+        }
+
+        g2d.setColor(Color.BLUE);
+        g2d.drawRect(100, 100, 100, 100);
+    }
+
+    void draw_quadtree(Graphics2D g2d, QuadTree quadtree) {
+        g2d.setColor(Color.WHITE);
+        g2d.drawRect((int) quadtree.boundary.x, (int) quadtree.boundary.y, (int) quadtree.boundary.width, (int) quadtree.boundary.height);
+        if (quadtree.devided) {
+            this.draw_quadtree(g2d, quadtree.north_east);
+            this.draw_quadtree(g2d, quadtree.north_west);
+            this.draw_quadtree(g2d, quadtree.south_east);
+            this.draw_quadtree(g2d, quadtree.south_west);
         }
     }
 
--- QuadTree.java
+++ QuadTree.java
@@ -1,14 +1,12 @@
-import java.awt.*;
-
 public class QuadTree {
-    Rectangle body;
+    Rectangle boundary;
     int max_points, points_length;
     boolean devided;
     Point[] points;
     QuadTree north_west, north_east, south_west, south_east;
 
     QuadTree(double x, double y, double width, double height, int max_points) {
-        body = new Rectangle(x, y, width, height);
+        boundary = new Rectangle(x, y, width, height);
         this.max_points = max_points;
         this.devided = false;
         this.points = new Point[max_points];
@@ -16,7 +14,7 @@ public class QuadTree {
     }
 
     void insert(Point point) {
-        if (this.body.intersects(point) && point != null) {
+        if (this.boundary.intersects(point) && point != null) {
             if (this.points_length < this.max_points) {
                 points[points_length] = point;
                 points_length++;
@@ -38,10 +36,10 @@ public class QuadTree {
     }
     
     void devide() {
-        this.north_west = new QuadTree(this.body.x, this.body.y, this.body.width / 2, this.body.height / 2, this.max_points);
-        this.north_east = new QuadTree(this.body.x + this.body.width / 2, this.body.y, this.body.width / 2, this.body.height / 2, this.max_points);
-        this.south_west = new QuadTree(this.body.x, this.body.y + this.body.height / 2, this.body.width / 2, this.body.height / 2, this.max_points);
-        this.south_east = new QuadTree(this.body.x + this.body.width / 2, this.body.y + this.body.height / 2, this.body.width / 2, this.body.height / 2, this.max_points);
+        this.north_west = new QuadTree(this.boundary.x, this.boundary.y, this.boundary.width / 2, this.boundary.height / 2, this.max_points);
+        this.north_east = new QuadTree(this.boundary.x + this.boundary.width / 2, this.boundary.y, this.boundary.width / 2, this.boundary.height / 2, this.max_points);
+        this.south_west = new QuadTree(this.boundary.x, this.boundary.y + this.boundary.height / 2, this.boundary.width / 2, this.boundary.height / 2, this.max_points);
+        this.south_east = new QuadTree(this.boundary.x + this.boundary.width / 2, this.boundary.y + this.boundary.height / 2, this.boundary.width / 2, this.boundary.height / 2, this.max_points);
 
         for (int i = 0; i < max_points; i++) {
             if (this.points[i] != null) {
@@ -50,17 +48,6 @@ public class QuadTree {
                 this.south_west.insert(this.points[i]);
                 this.south_east.insert(this.points[i]);
             }
-        }
-    }
-
-    void draw(Graphics2D g2d) {
-        g2d.setColor(Color.WHITE);
-        g2d.drawRect((int) this.body.x, (int) this.body.y, (int) this.body.width, (int) this.body.height);
-        if (devided) {
-            this.north_west.draw(g2d);
-            this.north_east.draw(g2d);
-            this.south_west.draw(g2d);
-            this.south_east.draw(g2d);
         }
     }
 }
