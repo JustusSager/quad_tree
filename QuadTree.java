92b1f52d3f2ea83892f0985192f2da269c59c81f
JustusSager <justus.sager@gmail.com>
2022-07-23 18:40:08 +0200

    implement rectangle class

--- QuadTree.java
+++ QuadTree.java
@@ -1,16 +1,14 @@
 import java.awt.*;
 
 public class QuadTree {
-    int x, y, width, height, max_points, points_length;
+    Rectangle body;
+    int max_points, points_length;
     boolean devided;
     Point[] points;
     QuadTree north_west, north_east, south_west, south_east;
 
-    QuadTree(int x, int y, int width, int height, int max_points) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    QuadTree(double x, double y, double width, double height, int max_points) {
+        body = new Rectangle(x, y, width, height);
         this.max_points = max_points;
         this.devided = false;
         this.points = new Point[max_points];
@@ -18,7 +16,7 @@ public class QuadTree {
     }
 
     void insert(Point point) {
-        if (this.intersects(point) && point != null) {
+        if (this.body.intersects(point) && point != null) {
             if (this.points_length < this.max_points) {
                 points[points_length] = point;
                 points_length++;
@@ -40,10 +38,10 @@ public class QuadTree {
     }
     
     void devide() {
-        this.north_west = new QuadTree(this.x, this.y, this.width / 2, this.height / 2, this.max_points);
-        this.north_east = new QuadTree(this.x + this.width / 2, this.y, this.width / 2, this.height / 2, this.max_points);
-        this.south_west = new QuadTree(this.x, this.y + this.height / 2, this.width / 2, this.height / 2, this.max_points);
-        this.south_east = new QuadTree(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, this.height / 2, this.max_points);
+        this.north_west = new QuadTree(this.body.x, this.body.y, this.body.width / 2, this.body.height / 2, this.max_points);
+        this.north_east = new QuadTree(this.body.x + this.body.width / 2, this.body.y, this.body.width / 2, this.body.height / 2, this.max_points);
+        this.south_west = new QuadTree(this.body.x, this.body.y + this.body.height / 2, this.body.width / 2, this.body.height / 2, this.max_points);
+        this.south_east = new QuadTree(this.body.x + this.body.width / 2, this.body.y + this.body.height / 2, this.body.width / 2, this.body.height / 2, this.max_points);
 
         for (int i = 0; i < max_points; i++) {
             if (this.points[i] != null) {
@@ -55,26 +53,9 @@ public class QuadTree {
         }
     }
 
-    boolean intersects(Point point) {
-        return (
-            point.x <= this.x + this.width &&
-            point.x >= this.x &&
-            point.y <= this.y + this.height &&
-            point.y >= this.y
-        );
-    }
-    boolean intersects(int x, int y, int width, int height) {
-        return !(
-            x > this.x + this.width ||
-            x + width < this.x ||
-            y > this.y + this.height ||
-            y + height < this.y
-        );
-    }
-
     void draw(Graphics2D g2d) {
         g2d.setColor(Color.WHITE);
-        g2d.drawRect(this.x, this.y, this.width, this.height);
+        g2d.drawRect((int) this.body.x, (int) this.body.y, (int) this.body.width, (int) this.body.height);
         if (devided) {
             this.north_west.draw(g2d);
             this.north_east.draw(g2d);
@@ -98,5 +79,33 @@ class Point {
         this.x = x;
         this.y = y;
         this.user_data = user_data;
+    }
+}
+
+class Rectangle {
+    double x, y, width, height;
+
+    Rectangle(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    boolean intersects(Point point) {
+        return (
+            point.x <= this.x + this.width &&
+            point.x >= this.x &&
+            point.y <= this.y + this.height &&
+            point.y >= this.y
+        );
+    }
+    boolean intersects(Rectangle rect) {
+        return !(
+            rect.x >= this.x + this.width ||
+            rect.x + rect.width <= this.x ||
+            rect.y >= this.y + this.height ||
+            rect.y + rect.height <= this.y
+        );
     }
 }
